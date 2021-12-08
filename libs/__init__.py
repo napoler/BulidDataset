2946b6315248088680e028066edad9c2bf095e19
Terry Chan <napoler2008@gmail.com>
2021-12-08 17:14:13 +0800

    labelst

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
-"""
-作者：　terrychan
-Blog: https://terrychan.org
-# 说明：
-
-"""
-
-if __name__ == '__main__':
-    pass
-=======
 from .AutoClear import AutoClear
->>>>>>> labelst
